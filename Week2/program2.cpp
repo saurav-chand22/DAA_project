ae1233fdd8f58668789d69fd75819adc547a4eb1
saurav-chand22 <chandsourav099@gmail.com>
2026-03-09 22:57:04 +0530

    Update program2.cpp

--- Week2/program2.cpp
+++ Week2/program2.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        bool found = false;
+
+        for(int k = n - 1; k >= 2 && !found; k--)
+        {
+            int i = 0;
+            int j = k - 1;
+
+            while(i < j)
+            {
+                int sum = arr[i] + arr[j];
+
+                if(sum == arr[k])
+                {
+                    cout << i + 1 << "," << j + 1 << "," << k + 1 << endl;
+                    found = true;
+                    break;
+                }
+                else if(sum < arr[k])
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+        }
+
+        if(!found)
+        {
+            cout << "No sequence found." << endl;
+        }
+    }
+
+    return 0;
+}
