2386858959857b08dc5963900ba4af0665f10b95
saurav-chand22 <chandsourav099@gmail.com>
2026-03-09 23:06:44 +0530

    Update program3.cpp

--- Week3/program3.cpp
+++ Week3/program3.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        sort(arr.begin(), arr.end());
+
+        bool duplicate = false;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(arr[i] == arr[i + 1])
+            {
+                duplicate = true;
+                break;
+            }
+        }
+
+        if(duplicate)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
