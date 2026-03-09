c5d539f21f62afb4116d7105b54592343823eb84
saurav-chand22 <chandsourav099@gmail.com>
2026-03-09 22:59:04 +0530

    Update program3.cpp

--- Week2/program3.cpp
+++ Week2/program3.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int k;
+        cin >> k;
+
+        int count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
+            {
+                if(abs(arr[i] - arr[j]) == k)
+                {
+                    count++;
+                }
+            }
+        }
+
+        cout << count << endl;
+    }
+
+    return 0;
+}
