d5a4539c73709b2befd325b6403c60f70e7cbf5e
saurav-chand22 <chandsourav099@gmail.com>
2026-03-09 23:02:47 +0530

    Update program2.cpp

--- Week3/program2.cpp
+++ Week3/program2.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int comparisons = 0;
+        int swaps = 0;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            int minIndex = i;
+
+            for(int j = i + 1; j < n; j++)
+            {
+                comparisons++;
+
+                if(arr[j] < arr[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+
+            if(minIndex != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+                swaps++;
+            }
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "comparisons = " << comparisons << endl;
+        cout << "swaps = " << swaps << endl;
+    }
+
+    return 0;
+}
