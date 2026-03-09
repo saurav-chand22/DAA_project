bea18bc7841a682c58568d3cce70ae4da5452940
saurav-chand22 <chandsourav099@gmail.com>
2026-03-09 23:01:33 +0530

    Update program1.cpp

--- Week3/program1.cpp
+++ Week3/program1.cpp
@@ -1,1 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        int comparisons = 0;
+        int shifts = 0;
+
+        for(int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0)
+            {
+                comparisons++;
+
+                if(arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    shifts++;
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            arr[j + 1] = key;
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "comparisons = " << comparisons << endl;
+        cout << "shifts = " << shifts << endl;
+    }
+
+    return 0;
+}
