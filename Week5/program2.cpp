5c36bd6acd5dc18b50bafab0df0ecd9acc03c3c7
saurav-chand22 <chandsourav099@gmail.com>
2026-05-21 15:16:26 +0530

    Update program2.cpp

--- Week5/program2.cpp
+++ Week5/program2.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while(T--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        int key;
+        cin >> key;
+
+        sort(arr, arr + n);
+
+        int left = 0;
+        int right = n - 1;
+
+        bool found = false;
+
+        while(left < right)
+        {
+            int sum = arr[left] + arr[right];
+
+            if(sum == key)
+            {
+                cout << arr[left] << " "
+                     << arr[right] << endl;
+
+                found = true;
+                break;
+            }
+            else if(sum < key)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        if(!found)
+        {
+            cout << "No Such Elements Exist"
+                 << endl;
+        }
+    }
+
+    return 0;
+}
